d0e4fb8af630daeaed44aa053f88b3e0d260db68
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-12-06 22:56:33 -0300

    Conectando ao Azure e enviando um imagem para o Storage

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using System.Text.RegularExpressions;
+using Azure.Storage.Blobs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+var azureStorageConnectionString = app.Configuration.GetValue<string>("AzureStorageConnectionString");
 
 var secrets = new Secrets();
 app.Configuration.GetSection("Secrets").Bind(secrets);
@@ -13,8 +18,35 @@ app.MapGet("/", () => new
   ApiUrl = app.Configuration.GetValue<string>("ApiUrl")
 });
 
+app.MapPost("/", (Upload model) =>
+{
+  var url = UploadBase64Image(model.Image, "user-images");
+  return Results.Ok(new
+  {
+    ImagemUrl = url
+  });
+});
+
+app.UseStaticFiles();
 app.Run();
 
+string UploadBase64Image(string base64Image, string container)
+{
+  var fileName = Guid.NewGuid().ToString() + ".jpg";
+  var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(base64Image, "");
+
+  byte[] imageBytes = Convert.FromBase64String(data);
+  var blobClient = new BlobClient(azureStorageConnectionString, container, fileName);
+
+  using (var stream = new MemoryStream(imageBytes))
+  {
+    blobClient.Upload(stream);
+  }
+
+  return blobClient.Uri.AbsoluteUri;
+}
+
+public record Upload(string Image);
 
 public class Secrets
 {
