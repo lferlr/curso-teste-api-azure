964d9753732b5dd75ab7a3fd7eca429449f48821
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-12-06 21:22:07 -0300

    Alterado as configs do App

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello my change is online!");
+var secrets = new Secrets();
+app.Configuration.GetSection("Secrets").Bind(secrets);
+
+var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+
+app.MapGet("/", () => new
+{
+  ConnectionString = connectionString,
+  Secrets = secrets
+});
 
 app.Run();
+
+
+public class Secrets
+{
+  public string JwtTokenSecret { get; set; } = string.Empty;
+  public string ApiKey { get; set; } = string.Empty;
+  public string PrivateKey { get; set; } = string.Empty;
+}
